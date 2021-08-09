fada02741fee5320ed964240e1c20dfb42f24034
Jenrielle Gaon <jenrielle@ubx.ph>
2021-08-10 03:19:28 +0800

    added helpers, custom theme and icon support

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { ColorModeScript } from '@chakra-ui/react';
+import { ColorModeScript, ChakraProvider, extendTheme } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { GlobalProvider } from './store/provider';
@@ -6,11 +6,50 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 
+const theme = extendTheme({
+  colors: {
+    normal: "#A8A878",
+    fighting: "#C03028",
+    flying: "#A890F0",
+    poison: "#A040A0",
+    ground: "#E0C068",
+    rock: "#B8A038",
+    bug: "#A8B820",
+    ghost: "#705898",
+    steel: "#B8B8D0",
+    fire: "#F08030",
+    water: "#6890F0",
+    grass: "#78C850",
+    electric: "#F8D030",
+    psychic: "#F85888",
+    ice: "#98D8D8",
+    dragon: "#7038F8",
+    dark: "#705848",
+    fairy: "#EE99AC",
+    unknown: "#68A090",
+    shadow: "#5b5b5b",
+    white: "#FFFFFF",
+    black: {
+      100: "#CDCDD0",
+      200: "#B4B4BA",
+      300: "#9B9AA2",
+      400: "#82818B",
+      500: "#686873",
+      600: "#504F5C",
+      700: "#363545",
+      800: "#1E1D2E",
+      900: "#040316",
+    }
+  },
+})
+
 ReactDOM.render(
   <StrictMode>
     <GlobalProvider>
-      <ColorModeScript />
-      <App />
+      <ChakraProvider theme={theme}>
+        <ColorModeScript />
+        <App />
+      </ChakraProvider>
     </GlobalProvider>
   </StrictMode>,
   document.getElementById('root')
